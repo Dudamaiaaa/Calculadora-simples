0f8517280e8d1128020d113e577bbff5b3dda49e
Kézia Andrade <anykeziaa@gmail.com>
2026-07-13 23:56:23 -0300

    commit 2: atualizar display e adicionar números

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let operadorEscolhido = null;
 
 const display = document.getElementById("display");
 const botoes = document.querySelectorAll("button");
+
+function atualizarDisplay() {
+  display.value = valorAtual;
+}
+
+function adicionarNumero(numero) {
+  if (numero === "." && valorAtual.includes(".")) return;
+
+  if (valorAtual === "0" && numero !== ".") {
+    valorAtual = numero;
+  } else {
+    valorAtual = valorAtual + numero;
+  }
+}
