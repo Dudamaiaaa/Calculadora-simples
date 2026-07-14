91020bf471c494edea67bc07cb09742872709517
Kézia Andrade <anykeziaa@gmail.com>
2026-07-13 23:51:42 -0300

    commit 1: criar script.js e estado

---
+++ script.js
@@ -0,0 +1,6 @@
+let valorAtual = "0";
+let valorAnterior = "";
+let operadorEscolhido = null;
+
+const display = document.getElementById("display");
+const botoes = document.querySelectorAll("button");
